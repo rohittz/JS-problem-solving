138819a2aec93e81e1a9e8ec02d5ac222656cd06
rohittz <r_o_h_i_t_roy@yahoo.com>
2021-01-22 19:47:48 +0600

    setting up

---
+++ assignment.js
@@ -0,0 +1,22 @@
+// github repository link
+// convert km to m
+function kilometerToMeter(km){
+    return m;
+}
+// budget calculator
+// per watch = 50, per phone = 100, per laptop = 500
+function budgetCalculator(nofWatch, nofPhone, nofLaptop){
+    return totalPrice;
+}
+// hotel cost
+// 1-10 night : 100TK
+// 11-20 night : 80TK
+// 20< night : 50TK
+function hotelCost(daysSpent){
+    return cost;
+}
+// megafriend
+// the largest friend name
+function megaFriend()
+
+// Check inputs for undefined things
